a85ed6ccb08b3c106e3bd7371a9125634fac589c
Sjur <sjursaa@gmail.com>
2024-11-12 20:59:50 +0100

    remove finished todo items

--- src/index.js
+++ src/index.js
@@ -3,13 +3,9 @@ import { pageload } from "./pageload.js";
 import { loadMenuPage } from "./menu.js";
 import { loadContactPage } from "./contact.js";
 
-// TODO: remove these lines
 pageload();
-console.log("hello world");
 
-// TODO: add tab switching in here
-// TODO: add event listeners to each navbar button
-//
+// TODO: Add color to current page button
 const homeButton = document.querySelector("#home");
 homeButton.onclick = () => {
   pageload();
